106c4086d65d9d2b3787da39b1cb86884ebb3104
KOVO-TECH <87145720+KOVO-TECH@users.noreply.github.com>
2021-08-04 09:30:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const client = new Discord.Client();
 
 const config = require("./config.json");
 const welcome = require("./welcome");
+const status = require("./config.json")
 
 client.on("ready", () => {
   console.log("Our Discord bot is online");
+  client.user.setActivity(status.status)
 
   welcome(client);
 });
